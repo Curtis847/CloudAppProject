0702b8c988099efa2059c595582c804efd9491e6
Curtis847 <murphy.curtis12@gmail.com>
2018-04-18 13:17:43 +0000

    Second Commit. Added Link to Patients in nav bar. Only shows up when logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'homepage/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :patients
-  root 'patients#index'
+  root 'homepage#index'
 end
